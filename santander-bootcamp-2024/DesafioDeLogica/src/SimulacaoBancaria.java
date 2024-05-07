a26242db16052a7ef598bb1be41e462a74a49b15
rbsaulodev <rb.saulodev@gmail.com>
2024-05-07 10:16:34 -0300

    ✨ feat: Adding program challenge: basic banking operations simulation

---
+++ santander-bootcamp-2024/DesafioDeLogica/src/SimulacaoBancaria.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+
+        while (true) {
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: "+saldo);
+                    break;
+                case 2:
+                    double valorSaque = scanner.nextDouble();
+                    if (valorSaque > saldo) {
+                        System.out.println("Saldo insuficiente.");
+                    } else {
+                        saldo -= valorSaque;
+                        System.out.println("Saldo atual: "+saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+}
