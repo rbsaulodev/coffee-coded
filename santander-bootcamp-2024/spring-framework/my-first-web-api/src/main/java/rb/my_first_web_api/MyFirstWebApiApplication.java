92ab5b43f13dfb86be08a9d858f0827301f2c3de
rbsaulodev <rb.saulodev@gmail.com>
2024-06-12 15:03:13 -0300

    :sparkles: feat(api): add first API implementation with Java Spring

---
+++ santander-bootcamp-2024/spring-framework/my-first-web-api/src/main/java/rb/my_first_web_api/MyFirstWebApiApplication.java
@@ -0,0 +1,12 @@
+package rb.my_first_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstWebApiApplication.class, args);
+	}
+}
---
+++ santander-bootcamp-2024/spring-framework/my-first-web-api/src/test/java/rb/my_first_web_api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rb.my_first_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
