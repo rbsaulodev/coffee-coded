b0ae2842c0fade31a40a7a1354daf29e15472ee3
rbsaulodev <rb.saulodev@gmail.com>
2024-06-13 17:52:48 -0300

    ✨ feat(api): add CRUD operations for user registration in Spring

---
+++ santander-bootcamp-2024/spring-framework/my-first-web-api/src/main/java/rb/my_first_web_api/model/Usuario.java
@@ -0,0 +1,47 @@
+package rb.my_first_web_api.model;
+
+public class Usuario {
+    private Integer id;
+    private String login;
+    private String password;
+
+    public Usuario() {}
+
+    public Usuario(Integer id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
