771d2e770705a2208b3dce7fe95645cdd24de7c1
rbsaulodev <rb.saulodev@gmail.com>
2024-06-14 09:12:18 -0300

    feat(security): add initial Spring Security configuration

---
+++ santander-bootcamp-2024/spring-framework/api-spring-security/src/main/java/rb/api_spring_security/ApiSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package rb.api_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ santander-bootcamp-2024/spring-framework/api-spring-security/src/test/java/rb/api_spring_security/ApiSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package rb.api_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
