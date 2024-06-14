6a95f3f37a2dffe1a073320e208d18c863137638
rbsaulodev <rb.saulodev@gmail.com>
2024-06-14 16:19:36 -0300

    ✨ feat: create initial Spring project with JWT token authentication

---
+++ santander-bootcamp-2024/spring-framework/primeira-api-jwt/src/main/java/rb/primeira_api_jwt/PrimeiraApiJwtApplication.java
@@ -0,0 +1,13 @@
+package rb.primeira_api_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraApiJwtApplication.class, args);
+	}
+
+}
---
+++ santander-bootcamp-2024/spring-framework/primeira-api-jwt/src/test/java/rb/primeira_api_jwt/PrimeiraApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package rb.primeira_api_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
