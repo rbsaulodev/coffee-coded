773b23dcf9004c7a2825267d703a59c207ccc112
rbsaulodev <rb.saulodev@gmail.com>
2024-06-19 17:02:50 -0300

    🎉 feat: Initialize REST API application for Users and Departments

---
+++ santander-bootcamp-2024/spring-framework/userdept/src/main/java/rb/userdept/UserdeptApplication.java
@@ -0,0 +1,13 @@
+package rb.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdeptApplication.class, args);
+	}
+
+}
---
+++ santander-bootcamp-2024/spring-framework/userdept/src/test/java/rb/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package rb.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
