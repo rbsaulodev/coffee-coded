334f9b8e99968185b6f933b47a265f9aeb4236de
rb <rb.saulodev@gmail.com>
2024-07-09 08:47:37 -0300

    🎉 feat: Start NLW Journey project Planner - a travel organization app
    - Initiated project setup for Planner
    - Set up basic dependencies and configurations
    - Started initial structure for travel planning features

---
+++ internet/planner-nlw/src/main/java/com/rocketseat/planner_nlw/PlannerNlwApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.planner_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerNlwApplication.class, args);
+	}
+
+}
---
+++ internet/planner-nlw/src/test/java/com/rocketseat/planner_nlw/PlannerNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.planner_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
