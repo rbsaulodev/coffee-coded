18c356b06ced312acdc61d5a76c3f244eb65adab
rb <rb.saulodev@gmail.com>
2024-07-17 23:06:03 -0300

    🐛 fix: Complete bag factory data listing task implementation
    - Fixed issues in the system to input and store details of 3 bags
    - Corrected attributes: code, color, brand, material type, value, and zipper presence
    - Corrected application of object-oriented concepts: objects, attributes, methods, class, and subclass
    - Properly utilized Scanner package for user input
    - Corrected printing of the list of bag characteristics after data entry

--- faculdade/lab-05/src/ClassHandBag.java
+++ faculdade/lab-05/src/ClassHandBag.java
@@ -1,15 +1,7 @@
 class ClassHandBag {
     String cor;
     String marca;
-    float valores[] = new float[3];
-
-    void status(){
-        System.out.println("A cor da bolsa é: " + this.cor);
-        System.out.println("A marca da bolsa é: " + this.marca);
-        System.out.print("Os valores da bolsa são: ");
-        for (float valor : this.valores) {
-            System.out.print(valor + " ");
-        }
-        System.out.println();
-    }
+    String material;
+    boolean ziper;
+    float valor;
 }
--- faculdade/lab-05/src/HandBag.java
+++ faculdade/lab-05/src/HandBag.java
@@ -3,20 +3,45 @@ import java.util.Scanner;
 public class HandBag {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ClassHandBag bolsa = new ClassHandBag();
+        ClassHandBag bolsa1 = new ClassHandBag();
+        ClassHandBag bolsa2 = new ClassHandBag();
+        ClassHandBag bolsa3 = new ClassHandBag();
+
+        System.out.println("Digite a cor da primeira bolsa: ");
+        bolsa1.cor = scan.nextLine();
+        System.out.println("Digite a cor da segunda bolsa: ");
+        bolsa2.cor = scan.nextLine();
+        System.out.println("Digite a cor da segunda bolsa: ");
+        bolsa3.cor = scan.nextLine();
         
-        System.out.println("Digite a cor da bolsa: ");
-        bolsa.cor = scan.nextLine();
-        System.out.println("Digite a marca da bolsa: ");
-        bolsa.marca = scan.nextLine();
-        System.out.println("Digite o primeiro valor da bolsa: ");
-        bolsa.valores[0] = scan.nextFloat();
-        System.out.println("Digite o segundo valor da bolsa: ");
-        bolsa.valores[1] = scan.nextFloat();
-        System.out.println("Digite o terceiro valor da bolsa: ");
-        bolsa.valores[2] = scan.nextFloat();
-        
+        System.out.println("Digite a marca da primeira bolsa: ");
+        bolsa1.marca = scan.nextLine();
+        System.out.println("Digite a marca da segunda bolsa: ");
+        bolsa2.marca = scan.nextLine();
+        System.out.println("Digite a marca da segunda bolsa: ");
+        bolsa3.marca = scan.nextLine();
+
+        System.out.println("Digite a material da primeira bolsa: ");
+        bolsa1.material = scan.nextLine();
+        System.out.println("Digite a material da segunda bolsa: ");
+        bolsa2.material = scan.nextLine();
+        System.out.println("Digite a cor da segunda bolsa: ");
+        bolsa3.material = scan.nextLine();
+
+        System.out.println("Tem ziper (true/false): ");
+        bolsa1.ziper = scan.nextBoolean();
+        System.out.println("Tem ziper (true/false): ");
+        bolsa2.ziper = scan.nextBoolean();
+        System.out.println("Tem ziper (true/false): ");
+        bolsa3.ziper = scan.nextBoolean();
+
+        System.out.println("Digite o valor da primeira bolsa: ");
+        bolsa1.valor = scan.nextFloat();
+        System.out.println("Digite o valor da primeira bolsa: ");
+        bolsa2.valor = scan.nextFloat();
+        System.out.println("Digite o valor da primeira bolsa: ");
+        bolsa3.valor = scan.nextFloat();
+
         scan.close();
-        bolsa.status();
     }
 }
