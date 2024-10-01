7296940c3adfe7b3f78f5769ffa290c449856130
rb-linux <rb.saulodev@gmail.com>
2024-10-01 09:57:31 -0300

    :sparkles: feat: initialize Anime Service project with Spring Boot setup

---
+++ internet/spring-boot-direto-das-trincheiras/anime-service/src/main/java/rb/AnimeServiceApplication.java
@@ -0,0 +1,13 @@
+package rb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeServiceApplication.class, args);
+	}
+
+}
---
+++ internet/spring-boot-direto-das-trincheiras/anime-service/src/test/java/rb/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
